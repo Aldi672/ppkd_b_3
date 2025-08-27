db77d62c3fdcfe30264e01f32074db1063a1b0c2
Aldi672 <phpdimas378@gmail.com>
2025-08-27 14:22:58 +0700

    Bab

--- lib/Tugas14/view/detail_user.dart
+++ lib/Tugas14/view/detail_user.dart
@@ -10,58 +10,118 @@ class DetailUser extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text(result.title ?? "Detail User")),
       body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomCenter,
+            end: Alignment.topRight,
+            colors: [
+              Color.fromARGB(255, 235, 186, 196),
+              Color.fromARGB(255, 240, 223, 230),
+              Color.fromARGB(255, 68, 66, 68),
+            ],
+          ),
+        ),
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                if (result.image != null)
-                  Center(child: Image.network(result.image!, height: 200)),
+                Container(
+                  width: double.infinity,
+                  height: 500,
+                  padding: EdgeInsets.only(left: 30, right: 30, bottom: 20),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image.network(result.image ?? "", fit: BoxFit.cover),
+                  ),
+                ),
                 SizedBox(height: 20),
-                Text(
-                  "Title: ${result.title ?? 'N/A'}",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+
+                Container(
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  child: Row(
+                    children: [
+                      Text(
+                        "${result.title ?? 'N/A'}",
+                        style: TextStyle(
+                          fontFamily: 'Latin',
+                          fontSize: 25,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Spacer(),
+                      CircleAvatar(
+                        child: Icon(Icons.home_filled, color: Colors.black),
+                      ),
+                      SizedBox(width: 15),
+                      CircleAvatar(
+                        child: Icon(
+                          Icons.volunteer_activism,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                SizedBox(height: 10),
-                Text(
-                  "original_title: ${result.originalTitle ?? 'N/A'}",
-                  style: TextStyle(fontSize: 16),
+
+                Container(
+                  padding: EdgeInsets.only(left: 20, right: 10),
+                  child: Text(
+                    "May 25,${result.releaseDate ?? 'N/A'}",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'Latin',
+                      fontWeight: FontWeight.w200,
+                    ),
+                  ),
                 ),
-                SizedBox(height: 10),
-                Text(
-                  "original_title_romanised: ${result.originalTitleRomanised?.toString() ?? 'N/A'}",
-                  style: TextStyle(fontSize: 16),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  "description: ${result.description ?? 'N/A'}",
-                  style: TextStyle(fontSize: 16),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  "movieBanner: ${result.movieBanner ?? 'N/A'}",
-                  style: TextStyle(fontSize: 16),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  "director: ${result.director ?? 'N/A'}",
-                  style: TextStyle(fontSize: 16),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  "producer: ${result.producer ?? 'N/A'}",
-                  style: TextStyle(fontSize: 16),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  "releaseDate: ${result.releaseDate ?? 'N/A'}",
-                  style: TextStyle(fontSize: 16),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  "runningTime: ${result.runningTime ?? 'N/A'}",
-                  style: TextStyle(fontSize: 16),
+                Container(
+                  padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "original_title: ${result.originalTitle ?? 'N/A'}",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "original_title_romanised: ${result.originalTitleRomanised?.toString() ?? 'N/A'}",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "description: ${result.description ?? 'N/A'}",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "movieBanner: ${result.movieBanner ?? 'N/A'}",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "director: ${result.director ?? 'N/A'}",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "producer: ${result.producer ?? 'N/A'}",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 10),
+
+                      SizedBox(height: 10),
+                      Text(
+                        "runningTime: ${result.runningTime ?? 'N/A'}",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
--- lib/Tugas14/view/get_api.dart
+++ lib/Tugas14/view/get_api.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_field
+
 import 'package:flutter/material.dart';
 import 'package:ppkd/Tugas14/api/get_user.dart';
 import 'package:ppkd/Tugas14/model/user_model.dart';
@@ -12,33 +14,88 @@ class Day23GetAPIScreen extends StatefulWidget {
 }
 
 class _Day23GetAPIScreenState extends State<Day23GetAPIScreen> {
+  final GlobalKey<RefreshIndicatorState> _refreshKey =
+      GlobalKey<RefreshIndicatorState>();
+
+  // ignore: prefer_final_fields
+  List<Result> _filteredBooks = [];
+  List<Result> _allBooks = [];
+  String _selectedFilter = 'title';
+  final TextEditingController _searchController = TextEditingController();
   @override
-  // void initState() {
-  //   super.initState();
-  //   getUser();
-  // }
+  void initState() {
+    super.initState();
+
+    _loadUsers();
+  }
+
+  Future<void> _loadUsers() async {
+    final users = await getUser();
+    setState(() {
+      _allBooks = users;
+      _filteredBooks = users;
+    });
+  }
+
+  void _filterBooks(String query) {
+    final filtered = _allBooks.where((users) {
+      final titleLower = users.title?.toLowerCase() ?? '';
+      final originalTitleLower = users.originalTitle?.toLowerCase() ?? '';
+      final searchLower = query.toLowerCase();
+
+      if (_selectedFilter == 'title') {
+        return titleLower.contains(searchLower);
+      } else if (_selectedFilter == 'original_title') {
+        return originalTitleLower.contains(searchLower);
+      }
+      return false;
+    }).toList();
+
+    setState(() {
+      _filteredBooks = filtered;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SafeArea(
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: RefreshIndicator(
+          key: _refreshKey,
+          onRefresh: _loadUsers,
           child: SingleChildScrollView(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Search by $_selectedFilter....",
+                      prefixIcon: Icon(Icons.search),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    onChanged: (value) {
+                      _filterBooks(value);
+                    },
+                  ),
+                ),
+
                 FutureBuilder<List<Result>>(
                   future: getUser(),
                   builder: (BuildContext context, AsyncSnapshot snapshot) {
                     if (snapshot.connectionState == ConnectionState.waiting) {
                       return CircularProgressIndicator();
                     } else if (snapshot.hasData) {
-                      final users = snapshot.data as List<Result>;
                       return ListView.builder(
                         physics: NeverScrollableScrollPhysics(),
                         shrinkWrap: true,
-                        itemCount: users.length,
+                        itemCount: _filteredBooks.length,
                         itemBuilder: (BuildContext context, int index) {
-                          final dataUser = users[index];
+                          final dataUser = _filteredBooks[index];
                           return Container(
                             padding: EdgeInsets.all(10),
                             child: ListTile(
